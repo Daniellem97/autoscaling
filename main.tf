30c81bcc8663a9dfc4bc21265f57769e8211944b
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-06-03 10:33:08 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.57.0"
+    }
+  }
+}
+
+module "my_workerpool" {
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v2.5.0"
+
+  configuration = <<-EOT
+    export SPACELIFT_TOKEN="${var.worker_pool_config}"
+    export SPACELIFT_POOL_PRIVATE_KEY="${var.worker_pool_private_key}"
+  EOT
+
+  min_size          = 1
+  max_size          = 5
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.security_groups
+  vpc_subnets       = var.vpc_subnets
+}
+
+variable "worker_pool_config" {
+  type = string
+}
+
+variable "nonprod_private_key" {
+  type = string
+}
+
+variable "worker_pool_id" {
+  type    = string
+  default = "01HK8GXR4C5PT465ZWBQHWXEA1"
+}
+
+variable "security_groups" {
+  type    = list(string)
+}
+
+variable "vpc_subnets" {
+  type    = list(string)
+}
+
+ variable "poweroff_delay" {
+   type = number
+   description = "number of seconds to wait before shutting down a failed instance"
+ }
+
+ variable "autoscaling_max_create" {
+  description = "The maximum number of instances the utility is allowed to create in a single run"
+  type        = number
+  default     = 2
+}
+
+variable "autoscaling_max_terminate" {
+  description = "The maximum number of instances the utility is allowed to terminate in a single run"
+  type        = number
+  default     = 1
+}
